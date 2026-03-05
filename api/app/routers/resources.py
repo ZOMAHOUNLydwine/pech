a4faa17e61c52c14a7f353f9eaac9979a569f359
dalini <dalinigba@gmail.com>
2026-03-05 12:13:27 +0100

    Nettoyage et mise à jour des fichiers api

--- api/app/routers/resources.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from typing import List
-
-router = APIRouter(prefix="/resources", tags=["resources"])
-
-@router.get("/")
-async def get_resources():
-    return {"resources": []}
